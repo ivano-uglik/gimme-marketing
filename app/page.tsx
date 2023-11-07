e0e65b18fafb33473699695237c53bfc096d696a
ivano-uglik <ivano.uglik7@gmail.com>
2023-11-07 12:09:54 +0100

    added primaryfeatures from template

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Hero } from "./Hero";
 import Navbar from "./Navbar";
+import { PrimaryFeatures } from "./PrimaryFeatures";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <div className="p-32 text-center">{" < section 2 > "}</div>
+      <PrimaryFeatures />
     </>
   );
 }
